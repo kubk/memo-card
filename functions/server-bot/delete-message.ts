7040b6884895db0afa4c4578b3bf709d8d70d8b5
Gorbachev Egor <7gorbachevm@gmail.com>
2024-01-04 12:29:10 +0700

    Add comment [skip ci]

--- functions/server-bot/delete-message.ts
+++ functions/server-bot/delete-message.ts
@@ -4,6 +4,7 @@ export const deleteMessage = async (ctx: Context) => {
   try {
     await ctx.deleteMessage();
   } catch (e) {
+    // If the message can't be deleted because it's too old, ignore the error
     console.error(e);
   }
 };
