bbd71768c03127ad1820d8c2d145f826dc200279
Gorbachev Egor <7gorbachevm@gmail.com>
2024-01-11 14:07:52 +0700

    Allow to provide custom colors for the card text

--- src/lib/sanitize-html/sanitize.ts
+++ src/lib/sanitize-html/sanitize.ts
@@ -2,9 +2,10 @@ import sanitizeHtml from "sanitize-html";
 
 export const sanitize = (text: string) => {
   return sanitizeHtml(text, {
-    allowedTags: ["small", "a", "big", "br", "b"],
+    allowedTags: ["small", "a", "big", "br", "b", "font"],
     allowedAttributes: {
       a: ["href"],
+      font: ["color"],
     },
   });
 };
