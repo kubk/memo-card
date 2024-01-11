933104df6829c4fe18e886054fe20db0f14bcfea
Gorbachev Egor <7gorbachevm@gmail.com>
2024-01-11 14:38:52 +0700

    Allow italic font in cards

--- src/lib/sanitize-html/sanitize.ts
+++ src/lib/sanitize-html/sanitize.ts
@@ -2,7 +2,7 @@ import sanitizeHtml from "sanitize-html";
 
 export const sanitize = (text: string) => {
   return sanitizeHtml(text, {
-    allowedTags: ["small", "a", "big", "br", "b", "font"],
+    allowedTags: ["small", "a", "big", "br", "b", "font", "i"],
     allowedAttributes: {
       a: ["href"],
       font: ["color"],
