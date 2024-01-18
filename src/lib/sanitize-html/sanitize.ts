e8f094b750f340897fc0a135d3517da79dd1beed
Gorbachev Egor <7gorbachevm@gmail.com>
2024-01-18 13:29:09 +0700

    Support the whole list of text formatting tags + list tags

--- src/lib/sanitize-html/sanitize.ts
+++ src/lib/sanitize-html/sanitize.ts
@@ -2,13 +2,36 @@ import sanitizeHtml from "sanitize-html";
 import { mathmlTagNames } from "mathml-tag-names";
 
 const allowedTags = [
-  "small",
-  "a",
-  "big",
-  "br",
-  "b",
-  "font",
-  "i",
+  // Text Formatting Tags
+
+  'a', // Link
+  "small", // Small text
+  "big", // Big text
+  "br", // Line break
+  'p', // Paragraph
+  "i", // Italic
+  "font", // Font for color
+  'h1', // Heading 1
+  'h2', // Heading 2
+  'h3', // Heading 3
+  'h4', // Heading 4
+  'h5', // Heading 5
+  'h6', // Heading 6
+  'em', // Emphasized text
+  'strong', // Strongly emphasized text
+  "b", // Bold
+  'mark', // Marked or highlighted text
+  'sub', // Subscript
+  'sup', // Superscript
+  'pre', // Preformatted text
+  'blockquote', // Blockquote
+  'q', // Inline quote
+
+  // List Tags
+  'ul', // Unordered list
+  'ol', // Ordered list
+  'li', // List item
+
   ...mathmlTagNames,
 ];
 
