a19beba1bfd9121a95807ec7f24c21b255fb99cf
Gorbachev Egor <7gorbachevm@gmail.com>
2024-02-15 14:42:46 +0700

    Don't wrap text in dropdown

--- src/ui/dropdown.tsx
+++ src/ui/dropdown.tsx
@@ -71,6 +71,7 @@ export const Dropdown = (props: Props) => {
               className={css({
                 padding: "12px 16px",
                 borderRadius: theme.borderRadius,
+                whiteSpace: 'nowrap',
                 ...tapScale,
                 ":hover": {
                   backgroundColor: theme.buttonColor,
