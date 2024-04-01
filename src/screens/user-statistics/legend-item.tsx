e91951b5ead35b64757390e341111fb75fbfeb4b
Gorbachev Egor <7gorbachevm@gmail.com>
2024-04-01 20:40:57 +0700

    Make statistics legend item more visible

--- src/screens/user-statistics/legend-item.tsx
+++ src/screens/user-statistics/legend-item.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/css";
 import React from "react";
+import { theme } from "../../ui/theme.tsx";
 
 type Props = {
   color: string;
@@ -14,6 +15,7 @@ export const LegendItem = (props: Props) => {
         width: 14,
         backgroundColor: color,
         borderRadius: 4,
+        border: `2px solid ${theme.bgColor}`,
       })}
     />
   );
