af1028be3d78c87c33a76bd404cc1c7ae590bb36
Gorbachev Egor <7gorbachevm@gmail.com>
2024-04-13 14:24:05 +0700

    Test patch-package
    Test patch-package

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./screens/app.tsx";
 import "./index.css";
-import "@xelene/tgui/dist/styles.css";
 import "@mdi/font/css/materialdesignicons.min.css";
 import WebApp from "@twa-dev/sdk";
 
