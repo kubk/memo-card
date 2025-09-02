0f6741a599a6ef3af31c64a9b0328bcc6d25017a
Egor G <7gorbachevm@gmail.com>
2025-09-02 22:16:55 +0700

    Voices (#131)
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/frontend/src/screens/deck-review/store/review-store-context.tsx
+++ packages/frontend/src/screens/deck-review/store/review-store-context.tsx
@@ -1,10 +1,16 @@
 import { createContext, ReactNode, useContext } from "react";
 import { ReviewStore } from "./review-store.ts";
 import { assert } from "api";
+import { useMount } from "../../../lib/react/use-mount.ts";
+import { loadHighQualityVoices } from "../../../lib/voice-playback/speak.ts";
 
 const Context = createContext<ReviewStore | null>(null);
 
 export const ReviewStoreProvider = (props: { children: ReactNode }) => {
+  useMount(() => {
+    loadHighQualityVoices();
+  });
+
   return (
     <Context.Provider value={new ReviewStore()}>
       {props.children}
