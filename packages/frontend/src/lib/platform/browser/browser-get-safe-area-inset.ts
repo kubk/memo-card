8a039c23e3d3c6561928b055a95c4dddb83ebbb7
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-25 15:05:38 +0700

    Hardocde safe are inset for standalone
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/frontend/src/lib/platform/browser/browser-get-safe-area-inset.ts
+++ packages/frontend/src/lib/platform/browser/browser-get-safe-area-inset.ts
@@ -5,22 +5,11 @@ function isStandaloneMode() {
   );
 }
 
-export function getSafeAreaValue(property: string): number {
-  const value = getComputedStyle(document.documentElement).getPropertyValue(
-    property,
-  );
-  const numValue = parseFloat(value);
-  return !isNaN(numValue) ? numValue : 0;
-}
-
 export function browserGetSafeAreaInset() {
   if (isStandaloneMode()) {
-    const safeAreaTop = getSafeAreaValue("env(safe-area-inset-top)");
-    const safeAreaBottom = 6;
-
     return {
-      top: safeAreaTop,
-      bottom: safeAreaBottom,
+      top: 0,
+      bottom: 6,
     };
   }
 
