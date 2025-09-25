28a41014a6f8408915ed32cc27add44bb9fee114
Gorbachev Egor <7gorbachevm@gmail.com>
2025-09-25 13:33:28 +0700

    Standalone mode inset
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/frontend/src/lib/platform/platform.ts
+++ packages/frontend/src/lib/platform/platform.ts
@@ -31,6 +31,7 @@ export interface Platform {
   getStartParam(): string | undefined;
   openInvoiceLink(link: string): void;
   haptic(type: HapticType): void;
+  getSafeAreaInset(): { top: number; bottom: number };
 }
 
 export type UseMainButtonType = (
