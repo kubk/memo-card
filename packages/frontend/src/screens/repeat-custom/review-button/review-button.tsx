7c1952943d8edfcba8fc726382d312c42bb6da2b
Gorbachev Egor <7gorbachevm@gmail.com>
2025-11-26 20:22:43 +0700

    Simplify the review button
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/frontend/src/screens/repeat-custom/review-button/review-button.tsx
+++ packages/frontend/src/screens/repeat-custom/review-button/review-button.tsx
@@ -1,19 +1,10 @@
-import { useState } from "react";
 import { screenStore } from "../../../store/screen-store.ts";
-import { BlocksIcon, BookOpenIcon, CirclePlayIcon } from "lucide-react";
+import { CirclePlayIcon } from "lucide-react";
 import { deckListStore } from "../../../store/deck-list-store.ts";
-import { BooleanToggle } from "mobx-form-lite";
 import { cn } from "../../../ui/cn.ts";
-import { t } from "../../../translations/t.ts";
-import { translateCardCount } from "../translate-card-count.ts";
 import { platform } from "../../../lib/platform/platform.ts";
-import { BottomSheet } from "../../../ui/bottom-sheet/bottom-sheet.tsx";
-import { Choice } from "../../deck-list/deck-or-folder-choose/choice.tsx";
-import { Flex } from "../../../ui/flex.tsx";
 
 export function ReviewButton() {
-  const [isMenuOpen] = useState(() => new BooleanToggle(false));
-
   if (deckListStore.myInfoRequest.isLoading || !deckListStore.myDecks.length) {
     return null;
   }
@@ -22,52 +13,13 @@ export function ReviewButton() {
     <div className={cn("fixed bottom-6 end-6")}>
       <button
         onClick={() => {
-          platform.haptic("medium");
-          isMenuOpen.toggle();
+          screenStore.go({ type: "reviewCustom" });
+          platform.haptic("light");
         }}
         className="h-14 pt-0.5 w-14 rounded-full bg-button text-white shadow-xl flex items-center justify-center z-20 active:scale-95"
       >
         <CirclePlayIcon className="h-8 w-8 active:scale-95 transition-transform" />
       </button>
-
-      <BottomSheet
-        isOpen={isMenuOpen.value}
-        onClose={() => isMenuOpen.setFalse()}
-      >
-        <Flex
-          direction={"column"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          mb={80}
-          gap={48}
-          fullWidth
-        >
-          <Flex fullWidth direction={"column"} gap={8}>
-            <Choice
-              icon={
-                <BookOpenIcon className="text-text self-center" size={18} />
-              }
-              title={t("review_all_due")}
-              description={translateCardCount(deckListStore.cardsToReviewCount)}
-              onClick={() => {
-                screenStore.go({ type: "reviewAll" });
-                platform.haptic("light");
-                isMenuOpen.setFalse();
-              }}
-            />
-            <Choice
-              icon={<BlocksIcon className="text-text self-center" size={18} />}
-              title={t("review_custom")}
-              description={translateCardCount(deckListStore.cardsTotalCount)}
-              onClick={() => {
-                screenStore.go({ type: "reviewCustom" });
-                platform.haptic("light");
-                isMenuOpen.setFalse();
-              }}
-            />
-          </Flex>
-        </Flex>
-      </BottomSheet>
     </div>
   );
 }
