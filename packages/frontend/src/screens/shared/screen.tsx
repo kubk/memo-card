9799659a8059af4af5fffbf2fb2a449c0d6c9575
Gorbachev Egor <7gorbachevm@gmail.com>
2026-06-25 23:11:35 +0700

    Fix screen regression
    This commit was filtered by https://github.com/kubk/git-filter
    Some files were excluded, so this commit may appear empty or incomplete.

--- packages/frontend/src/screens/shared/screen.tsx
+++ packages/frontend/src/screens/shared/screen.tsx
@@ -17,8 +17,12 @@ export function Screen(props: Props) {
   if (platform instanceof TelegramPlatform) {
     if (platform.isMobile()) {
       return (
-        <div className="mb-4 flex flex-col gap-2 pb-[calc(var(--tg-content-safe-area-inset-bottom,0px)_+_4px)] pl-[calc(var(--tg-content-safe-area-inset-left,0px)_+_4px)] pr-[calc(var(--tg-content-safe-area-inset-right,0px)_+_4px)]">
-          {title && <h3 className="text-center text-lg">{title}</h3>}
+        <div className="relative mb-4 flex flex-col gap-2 pb-[calc(var(--tg-content-safe-area-inset-bottom,0px)_+_4px)] pl-[calc(var(--tg-content-safe-area-inset-left,0px)_+_4px)] pr-[calc(var(--tg-content-safe-area-inset-right,0px)_+_4px)]">
+          {title && (
+            <h3 className="absolute inset-x-0 top-[calc(10px_-_var(--app-top-offset,12px))] text-center text-lg">
+              {title}
+            </h3>
+          )}
           {subtitle}
           {children}
         </div>
